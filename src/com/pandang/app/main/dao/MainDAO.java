e35e63c1ad0690989b9ff3bb38885c4dcb984d11
MichealChung <jst6227@naver.com>
2023-04-06 13:32:53 +0900

    슬라이드 구현 팔로우 제외 판매글 jsp 구현 단계 ㅋ

--- src/com/pandang/app/main/dao/MainDAO.java
+++ src/com/pandang/app/main/dao/MainDAO.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 
 import com.mybatis.config.MyBatisConfig;
+import com.pandang.app.main.dto.MainDTO;
+import com.pandang.app.main.vo.MainFollowVO;
+import com.pandang.app.main.vo.MainLikeViewVO;
 import com.pandang.app.main.vo.MainVO;
 
 public class MainDAO {
@@ -16,7 +19,27 @@ public class MainDAO {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
 	
-	public List<MainVO> selectAll(Map<String, Integer> pageMap){
-		return null;
+	public List<MainVO> selectAll(){
+		return sqlSession.selectList("main.selectAll");
 	}
+	
+	public List<MainLikeViewVO> select(){
+		return sqlSession.selectList("main.select");
+	}
+
+//	public List<MainDTO> select(){
+//		return sqlSession.selectList("main.select");
+//	}
+//	public void follow(MainFollowVO mainFollowVO){
+//		sqlSession.insert("MainFollow.insert", mainFollowVO);
+//	}
+	
+//	public  MainDTO selectOne(MainDTO mainDTO){
+//		sqlSession.selectOne("main.selectOne", mainDTO);
+	
+	
+//	public MainDTO selectChannel(MainDTO mainDTO) {
+//		return sqlSession.selectOne("main.selectOne", mainDTO);
+//	}
+	
 }
--- src/com/pandang/app/main/vo/MainVO.java
+++ src/com/pandang/app/main/vo/MainVO.java
@@ -1,7 +1,102 @@
 package com.pandang.app.main.vo;
 
+/*
+ *	member_number int unsigned auto_increment primary key,
+ * member_nickname varchar(300),
+ * channel_comment varchar(300),
+ * channel_number int,
+ * channel_name  varchar(300),
+ * channel_file_system_name varchar(300),
+ * member_number_to int
+ * member_number_from int
+ */
+
 public class MainVO {
-	int memberNumber;
-	String memberNickname;
-	String channelComment;
+	private int memberNumber;
+	private String memberNickname;
+	private int memberNumberTo;
+	private int memberNumberFrom;
+	private String channelComment;
+	private int channelNumber;
+	private String channelName;
+	private String channelFileSystemName;
+
+	public MainVO() {}
+
+	public int getMemberNumber() {
+		return memberNumber;
+	}
+
+	public void setMemberNumber(int memberNumber) {
+		this.memberNumber = memberNumber;
+	}
+
+	public String getMemberNickname() {
+		return memberNickname;
+	}
+
+	public void setMemberNickname(String memberNickname) {
+		this.memberNickname = memberNickname;
+	}
+
+	public int getMemberNumberTo() {
+		return memberNumberTo;
+	}
+
+	public void setMemberNumberTo(int memberNumberTo) {
+		this.memberNumberTo = memberNumberTo;
+	}
+
+	public int getMemberNumberFrom() {
+		return memberNumberFrom;
+	}
+
+	public void setMemberNumberFrom(int memberNumberFrom) {
+		this.memberNumberFrom = memberNumberFrom;
+	}
+
+	public String getChannelComment() {
+		return channelComment;
+	}
+
+	public void setChannelComment(String channelComment) {
+		this.channelComment = channelComment;
+	}
+
+	public int getChannelNumber() {
+		return channelNumber;
+	}
+
+	public void setChannelNumber(int channelNumber) {
+		this.channelNumber = channelNumber;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getChannelFileSystemName() {
+		return channelFileSystemName;
+	}
+
+	public void setChannelFileSystemName(String channelFileSystemName) {
+		this.channelFileSystemName = channelFileSystemName;
+	}
+
+	@Override
+	public String toString() {
+		return "MainVO [memberNumber=" + memberNumber + ", memberNickname=" + memberNickname + ", memberNumberTo="
+				+ memberNumberTo + ", memberNumberFrom=" + memberNumberFrom + ", channelComment=" + channelComment
+				+ ", channelNumber=" + channelNumber + ", channelName=" + channelName + ", channelFileSystemName="
+				+ channelFileSystemName + "]";
+	}
+
+	
+	
 }
+
+	
