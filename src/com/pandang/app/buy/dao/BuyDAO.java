412e2179137f73aa823a1b1589f7d90107241153
lee-geon-hee <lkh666666@nate.com>
2023-04-24 13:24:45 +0900

    0424

--- src/com/pandang/app/buy/dao/BuyDAO.java
+++ src/com/pandang/app/buy/dao/BuyDAO.java
@@ -27,7 +27,8 @@ public class BuyDAO {
 	
 	public void updatePost(BuyVO buyVO) {
 		sqlSession.update("buy.updatePost", buyVO);
-
+	}
+	
 	public void buyInsert(BuyDTO buyDTO) {
 		sqlSession.insert("buy.buyInsert", buyDTO);
 	}
